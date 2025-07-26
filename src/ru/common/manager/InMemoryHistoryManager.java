0273f2a0a5498f4fa8b632ed982d03e7bb2f6912
Mariya Imaeva <m.imaeva@corp.mail.ru>
2025-07-27 00:02:58 +0300

    sprint-6-final-task

--- src/ru/common/manager/InMemoryHistoryManager.java
+++ src/ru/common/manager/InMemoryHistoryManager.java
@@ -1,28 +1,95 @@
 package ru.common.manager;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import ru.common.model.Task;
 
 public class InMemoryHistoryManager implements HistoryManager {
 
-    List<Task> historyEntities = new ArrayList<>();
-    static final int MAX_REQUESTED_ENTITIES = 10;
+    private final HashMap<Integer, Node> historyEntities = new HashMap<>();
+    private Node head;
+    private Node<Task> tail;
 
     @Override
     public void add(Task task) {
         if (task == null) {
             return;
         }
-        if (historyEntities.size() >= MAX_REQUESTED_ENTITIES) {
-            historyEntities.removeFirst();
+
+        if (historyEntities.containsKey(task.getId())) {
+            removeNode(historyEntities.get(task.getId()));
         }
-        historyEntities.add(task);
+        linkLast(task);
     }
 
     @Override
     public List<Task> getHistory() {
-        return List.copyOf(historyEntities);
+        return new ArrayList<>(getTasks());
+    }
+
+    @Override
+    public void remove(int id) {
+        if (!historyEntities.containsKey(id)) {
+            return;
+        }
+
+        Node node = historyEntities.remove(id);
+
+        if (node == null) {
+            return;
+        }
+
+        Node prev = node.prev;
+        Node next = node.next;
+
+        if (prev != null) {
+            prev.next = next;
+        } else {
+            head = next;
+        }
+
+        if (next != null) {
+            next.prev = prev;
+        } else {
+            tail = prev;
+        }
+    }
+
+    private void removeNode(Node node) {
+        if (node.prev != null) {
+            node.prev.next = node.next;
+        } else {
+            head = node.next;
+        }
+        if (node.next != null) {
+            node.next.prev = node.prev;
+        } else {
+            tail = node.prev;
+        }
+    }
+
+
+    private void linkLast(Task task) {
+        Node<Task> newNode = new Node<>(tail, task, null);
+        if (tail == null) {
+            head = newNode;
+        } else {
+            tail.next = newNode;
+        }
+        tail = newNode;
+        historyEntities.put(task.getId(), newNode);
+
+    }
+
+    public List<Task> getTasks() {
+        List<Task> result = new ArrayList<>();
+        Node<Task> current = head;
+        while (current != null) {
+            result.add(current.data);
+            current = current.next;
+        }
+        return result;
     }
 }
