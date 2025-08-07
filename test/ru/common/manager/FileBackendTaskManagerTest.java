00c3570591f8b59aa5afa836b786d7d3e107a0ec
Mariya Imaeva <m.imaeva@corp.mail.ru>
2025-08-08 01:17:55 +0300

    sprint-7-final-task

--- test/ru/common/manager/FileBackendTaskManagerTest.java
+++ test/ru/common/manager/FileBackendTaskManagerTest.java
@@ -3,9 +3,10 @@ package ru.common.manager;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.List;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import ru.common.model.Epic;
@@ -17,14 +18,24 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class FileBackendTaskManagerTest {
 
+    private Path tempDir;
+    private Path tempFile;
+
+    @BeforeEach
+    public void setUp() throws IOException {
+        tempDir = Files.createTempDirectory("kanbanTest");
+        tempFile = Files.createTempFile(tempDir, "test", ".txt");
+    }
+
+    @AfterEach
+    public void tearDown() throws IOException {
+        Files.deleteIfExists(tempFile);
+        Files.deleteIfExists(tempDir);
+    }
+
     @Test
     public void testSaveToFile() throws IOException {
-        Path specificDirectory = Paths.get("/Users/m-imaeva/IdeaProjects/java-kanban/");
-        if (!Files.exists(specificDirectory)) {
-            Files.createDirectories(specificDirectory);
-        }
-        Path file = Files.createTempFile(specificDirectory, "test", ".txt");
-        FileBackedTaskManager manager = new FileBackedTaskManager(file);
+        FileBackedTaskManager manager = new FileBackedTaskManager(tempFile);
 
         Task task = new Task("Task", "", TaskStatus.NEW);
         Epic epic = new Epic("Epic", "");
@@ -32,11 +43,9 @@ public class FileBackendTaskManagerTest {
         manager.createEpic(epic);
         manager.createTask(task);
 
-        List<String> lines = Files.readAllLines(file);
+        List<String> lines = Files.readAllLines(tempFile);
         assertEquals(2, lines.size(), "Должно быть сохранено 2 элемента");
         assertTrue(lines.contains(manager.toString(task)));
         assertTrue(lines.contains(manager.toString(epic)));
-
-        Files.deleteIfExists(file);
     }
 }
